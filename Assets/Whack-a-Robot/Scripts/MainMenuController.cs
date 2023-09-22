f77d2274a166633f936db81ae838fcce02633290
Cj Karinen <42248595+cjkarinen20@users.noreply.github.com>
2023-09-22 18:06:49 -0400

    Housekeeping Update
    Organized and purged assets and files, tweaked character movement/IK Rig, fixed character head clipping with camera, added collision to menu scene

--- Assets/Whack-a-Robot/Scripts/MainMenuController.cs
+++ Assets/Whack-a-Robot/Scripts/MainMenuController.cs
@@ -5,23 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    [SerializeField]
-    public float defaultHeight = 1.8f;
 
-    [SerializeField]
-    public Camera camera;
 
-    private void Resize()
-    {
-        float headHeight = camera.transform.localPosition.y;
-        float scale = defaultHeight / headHeight;
-        transform.localScale = Vector3.one * scale;
-
-    }
-    private void OnEnable()
-    {
-        Resize();
-    }
     private void PlayButton()
     {
         Debug.Log("Main Gameplay Scene Loaded");
